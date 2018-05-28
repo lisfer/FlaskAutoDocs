6a382b08cd0a6cc76450a56a01b63897105adb33
lisfer <makrushinds@gmail.com>
2018-05-29 00:51:03 +0300

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='FlaskAutoDocs',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
