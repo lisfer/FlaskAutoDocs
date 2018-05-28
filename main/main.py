4f65e36873d005e6f3e37c060d1b7e6f6e415ed4
lisfer <makrushinds@gmail.com>
2018-05-29 00:11:04 +0300

    collect info from docstring andl flask urls

---
+++ main/main.py
@@ -0,0 +1,63 @@
+import re
+
+
+class FlaskAutoDoc:
+
+    RE_GET_RETURN = re.compile(r'(?<=:return:)\s*(.*)', re.DOTALL)
+    RE_GET_DESCRIPTION = re.compile(r'\s+(?P<data>.+?)?\s+(?=:params|:api_param|:return|$)', re.DOTALL)
+    RE_GET_API_PARAMS = re.compile(
+        r'(?<=:api_param) ((?P<p_name>.+?): (?P<p_descr>.+?))\s+(?=:params|:api_param|:return|$)', re.DOTALL)
+
+    def __init__(self, flask_app):
+        self._data = dict()
+        self.flask_app = flask_app
+
+    def doc(self, func):
+        self._data[func.__name__] = func
+        return func
+
+    @classmethod
+    def default_description(self):
+        return {'description': '', 'url':'', 'methods': set(), 'return': '', 'params': dict()}
+
+    def get(self, name):
+        f_descr = self.default_description()
+        func = self._data.get(name)
+
+        if not func:
+            return f_descr
+
+        if func.__doc__:
+            f_descr['description'] = self._get_description(func.__doc__)
+            f_descr['return'] = self._get_return(func.__doc__)
+            f_descr['params'] = self._get_params(func.__doc__)
+
+        f_descr['url'] = self._get_url(func)
+        f_descr['methods'] = self._get_methods(func)
+        return f_descr
+
+    def _get_route_rule(self, name):
+        return self.flask_app.url_map._rules_by_endpoint.get(name)
+
+    def _get_url(self, func):
+        rule = self._get_route_rule(func.__name__)
+        return rule[0].rule if rule else ''
+
+    def _get_methods(self, func):
+        rule = self._get_route_rule(func.__name__)
+        return rule[0].methods if rule else ''
+
+    @classmethod
+    def _get_description(cls, docstring):
+        data = cls.RE_GET_DESCRIPTION.search(docstring) or ''
+        return data.groups('data')[0] if data else ''
+
+    @classmethod
+    def _get_return(cls, docstring):
+        return (cls.RE_GET_RETURN.findall(docstring)[0] or '').strip()
+
+    @classmethod
+    def _get_params(cls, docstring):
+        data = {m.group('p_name'): m.group('p_descr') for m in cls.RE_GET_API_PARAMS.finditer(docstring)}
+        return data
+
---
+++ tests/tests.py
@@ -0,0 +1,75 @@
+import unittest
+
+from flask import Flask, jsonify
+from main import FlaskAutoDoc
+
+
+
+def fixture_simple():
+    pass
+
+
+def fixture_full(person_id:int):
+    """
+    Function changes name and age of the person
+    it its' cool: it can do amazing stuff
+
+    and so on
+    :params person_id: id of the person
+    :api_param name: name of the person to be set
+    :api_param age: new age for the person
+    :return: true of false
+        and many other interesting stuff
+    """
+
+def fixture_mixing(person_id:int):
+    """
+    Function changes name and age of the person
+    it its' cool: it can do amazing stuff
+
+    and so on
+    :params person_id: id of the person
+    :api_param name: name of the person to be set
+        api_param row 2
+    :params test1: test
+        params row 2
+    :api_param age: new age for the person
+    :params test2: test
+    :return: true of false
+        and many other interesting stuff
+    """
+
+
+class FlaskAutoDocTest(unittest.TestCase):
+
+    def setUp(self):
+        app = Flask(__name__)
+        self.fad = FlaskAutoDoc(app)
+        app.route('/api')(self.fad.doc(fixture_simple))
+        app.route('/api/person/<int:person_id>', methods=['POST'])(self.fad.doc(fixture_full))
+        app.route('/api/person/<int:person_id>', methods=['POST'])(self.fad.doc(fixture_mixing))
+
+    def test_empty_function(self):
+        fs = self.fad.get('fixture_simple')
+        assert fs['url'] == '/api'
+        assert fs['description'] == ''
+        assert fs['methods'] == {'HEAD', 'OPTIONS', 'GET'}
+        assert fs['return'] == ''
+        assert fs['params'] == {}
+
+    def test_described_function(self):
+        ff = self.fad.get('fixture_full')
+        assert ff['url'] == '/api/person/<int:person_id>'
+        assert ff['description'] == "Function changes name and age of the person\n    it its' cool: it can do amazing stuff\n\n    and so on"
+        assert ff['methods'] == {'POST', 'OPTIONS'}
+        assert ff['return'] == 'true of false\n        and many other interesting stuff'
+        assert ff['params'] == {'name': 'name of the person to be set', 'age': 'new age for the person'}
+
+    def test_mixed_function(self):
+        ff = self.fad.get('fixture_mixing')
+        assert ff['url'] == '/api/person/<int:person_id>'
+        assert ff['description'] == "Function changes name and age of the person\n    it its' cool: it can do amazing stuff\n\n    and so on"
+        assert ff['methods'] == {'POST', 'OPTIONS'}
+        assert ff['return'] == 'true of false\n        and many other interesting stuff'
+        assert ff['params'] == {'name': 'name of the person to be set\n        api_param row 2', 'age': 'new age for the person'}
+
