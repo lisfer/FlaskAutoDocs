34a812903796898f3a848b43b645e811f78d76dc
lisfer <makrushinds@gmail.com>
2018-05-29 11:57:29 +0300

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='FlaskAutoDocs',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     description='Documentation generator for flask',
     long_description=open(join(dirname(__file__), 'README.md')).read(),
